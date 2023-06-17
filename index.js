dbdf8893597bb8bb661e12fa34af7bf8def312b7
Brandi L Mason <masonbrandi66@gmail.com>
2023-06-17 14:29:49 -0600

    round two of event listening lab, completed

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
 }
+addingEventListener();
+
+
+// Other way that the test passed but no alert was showing on webpage.
+
+// function addingEventListener(){
+//     const input = document.getElementById('button');
+//     function clickAlert(){
+//         alert('I was clicked!');
+//     }
+//     input.addEventListener('click', clickAlert);
+// }
+
+// console.log(addEventListener())
